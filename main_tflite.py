e15d7d50c3de8c64152b99382bc9bfbba93d8a27
MarcelWilnicki <marcel@onspecta.com>
2021-06-07 23:54:31 +0200

    wip

--- main_tflite.py
+++ main_tflite.py
@@ -2,7 +2,10 @@ import tensorflow as tf
 import numpy as np
 import io
 import csv
+import os
 
+from scipy.io import wavfile
+from scipy.signal import resample
 # Download the model to yamnet.tflite
 # path on graviton inside docker
 interpreter = tf.lite.Interpreter('/onspecta/dev/knowles_benchmark/lite-model_yamnet_tflite_1.tflite')
@@ -18,6 +21,15 @@ def class_names_from_csv(class_map_csv_text):
 
     return class_names
 
+def ensure_sample_rate(original_sample_rate, waveform,
+                       desired_sample_rate=16000):
+    """Resample waveform if required."""
+    if original_sample_rate != desired_sample_rate:
+        desired_length = int(round(float(len(waveform)) /
+                                   original_sample_rate * desired_sample_rate))
+        waveform = resample(waveform, desired_length)
+    return desired_sample_rate, waveform
+
 input_details = interpreter.get_input_details()
 waveform_input_index = input_details[0]['index']
 output_details = interpreter.get_output_details()
@@ -27,6 +39,15 @@ spectrogram_output_index = output_details[2]['index']
 
 # Input: 3 seconds of silence as mono 16 kHz waveform samples.
 waveform = np.zeros(3 * 16000, dtype=np.float32)
+
+# wav_file_name = 'sounds/cough.wav'
+# print(wav_file_name)
+# sample_rate, wav_data = wavfile.read(wav_file_name, 'rb')
+# sample_rate, wav_data = ensure_sample_rate(sample_rate, wav_data)
+#
+# waveform = wav_data / tf.int16.max
+#
+# print(type(waveform))
 
 interpreter.resize_tensor_input(waveform_input_index, [len(waveform)], strict=True)
 interpreter.allocate_tensors()

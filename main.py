3dff8296e64131732ca1c47c655537166f4295ed
MarcelWilnicki <marcel@onspecta.com>
2021-06-07 14:29:16 +0200

    wip

--- main.py
+++ main.py
@@ -4,81 +4,95 @@ import numpy as np
 import csv
 import time
 import os
+import argparse
 
 from IPython.display import Audio
 from scipy.io import wavfile
 from scipy.signal import resample
 
-# model documentation: https://tfhub.dev/google/yamnet/1
 
-# Load the model.
-model = hub.load('https://tfhub.dev/google/yamnet/1')
-
-tf.config.threading.set_intra_op_parallelism_threads(1)
-tf.config.threading.set_inter_op_parallelism_threads(1)
-
-def class_names_from_csv(class_map_csv_text):
-    """Returns list of class names corresponding to score vector."""
-    class_names = []
-    with tf.io.gfile.GFile(class_map_csv_text) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            class_names.append(row['display_name'])
-
-    return class_names
+# rsync for graviton
+# alias temp_sync=
+# "rsync -a --exclude "__pycache__/" /home/marcel/dev/knowles_benchmark/ graviton:/home/marcel/dev/knowles_benchmark/"
 
 
-def ensure_sample_rate(original_sample_rate, waveform,
-                       desired_sample_rate=16000):
-    """Resample waveform if required."""
-    if original_sample_rate != desired_sample_rate:
-        desired_length = int(round(float(len(waveform)) /
-                                   original_sample_rate * desired_sample_rate))
-        waveform = resample(waveform, desired_length)
-    return desired_sample_rate, waveform
+def parse_args():
+    parser = argparse.ArgumentParser(description="Knowles benchmark.")
+    parser.add_argument("--intra",
+                        type=int, default=1, required=False)
+    parser.add_argument("--inter",
+                        type=int, default=1, required=False)
+    return parser.parse_args()
 
 
-class_map_path = model.class_map_path().numpy()
-class_names = class_names_from_csv(class_map_path)
+def main():
+    args = parse_args()
 
-total_time = 0.0
-count = 0
-# loop through sound files
-for file in os.listdir('/onspecta/dev/knowles_benchmark/sounds/'):
-    if file.endswith('.wav'):
-        wav_file_name = 'sounds/' + file
-        print(wav_file_name)
-        sample_rate, wav_data = wavfile.read(wav_file_name, 'rb')
-        sample_rate, wav_data = ensure_sample_rate(sample_rate, wav_data)
+    # model documentation: https://tfhub.dev/google/yamnet/1
 
-        # Show some basic information about the audio.
-        # duration = len(wav_data) / sample_rate
-        # print(f'Sample rate: {sample_rate} Hz')
-        # print(f'Total duration: {duration:.2f}s')
-        # print(f'Size of the input: {len(wav_data)}')
+    tf.config.threading.set_intra_op_parallelism_threads(args.intra)
+    tf.config.threading.set_inter_op_parallelism_threads(args.inter)
 
-        # The wav_data needs to be normalized to values in [-1.0, 1.0]
-        waveform = wav_data / tf.int16.max
+    # Load the model.
+    model = hub.load('https://tfhub.dev/google/yamnet/1')
 
-        # Run the model, check the output.
-        start = time.time()
-        scores, embeddings, spectrogram = model(waveform)
-        finish = time.time()
+    def class_names_from_csv(class_map_csv_text):
+        """Returns list of class names corresponding to score vector."""
+        class_names = []
+        with tf.io.gfile.GFile(class_map_csv_text) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                class_names.append(row['display_name'])
 
-        inference_time = finish - start
-        total_time += inference_time
+        return class_names
 
-        scores_np = scores.numpy()
-        spectrogram_np = spectrogram.numpy()
-        infered_class = class_names[scores_np.mean(axis=0).argmax()]
-        print(f'The main sound is: {infered_class}')
+    def ensure_sample_rate(original_sample_rate, waveform,
+                           desired_sample_rate=16000):
+        """Resample waveform if required."""
+        if original_sample_rate != desired_sample_rate:
+            desired_length = int(round(float(len(waveform)) /
+                                       original_sample_rate * desired_sample_rate))
+            waveform = resample(waveform, desired_length)
+        return desired_sample_rate, waveform
 
-        count += 1
-        continue
+    class_map_path = model.class_map_path().numpy()
+    class_names = class_names_from_csv(class_map_path)
 
-print("*" * 45)
-print(f'Total time was: {total_time} seconds')
-print(f'Average inference time was: {total_time/count} seconds')
-print(f'the measure was performed on: {count} samples')
+    total_time = 0.0
+    count = 0
 
+    for file in os.listdir('/onspecta/dev/knowles_benchmark/sounds/'):
+        if file.endswith('.wav'):
+            wav_file_name = 'sounds/' + file
+            print(wav_file_name)
+            sample_rate, wav_data = wavfile.read(wav_file_name, 'rb')
+            sample_rate, wav_data = ensure_sample_rate(sample_rate, wav_data)
+
+            # The wav_data needs to be normalized to values in [-1.0, 1.0]
+            waveform = wav_data / tf.int16.max
+
+            # Run the model, check the output.
+            start = time.time()
+            scores, embeddings, spectrogram = model(waveform)
+            finish = time.time()
+
+            inference_time = finish - start
+            total_time += inference_time
+
+            scores_np = scores.numpy()
+            spectrogram_np = spectrogram.numpy()
+            infered_class = class_names[scores_np.mean(axis=0).argmax()]
+            print(f'The main sound is: {infered_class}')
+
+            count += 1
+            continue
+
+    print("*" * 45)
+    print(f'Total time was: {total_time} seconds')
+    print(f'Average inference time was: {total_time/count} seconds')
+    print(f'the measure was performed on: {count} samples')
+
+
+if __name__ == "__main__":
+    main()
 
